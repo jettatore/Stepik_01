2929a8dd36bd17c1358bfb9d09916e928a3d77ba
jettarider <jettarider@gmail.com>
2018-04-10 08:47:09 +0300

    changed

--- String.cpp
+++ String.cpp
@@ -89,43 +89,10 @@ struct String {
 };
 
 
-String & fun1(String & s)
-{
-	return s;
-}
 
-String && fun2(String & s)
-{
-	return std::move(s);
-}
-
-String fun3(String & s)
-{
-	return std::move(s);
-}
-
-String fun4(String s)
-{
-	return std::move(s);
-}
-
-String fun5(String s)
-{
-	return std::forward<String>(s);
-}
-
-String && fun6(String && s)
-{
-	return std::move(s);
-}
-
-String fun7(String && s)
-{
-	return s;
-}
 
 int main() {
-	/*
+	
 	String s1 = String("Hellow");
 	String s2("world");
 	//String s3 = s1 + ", " + s2 + " !";
@@ -133,30 +100,7 @@ int main() {
 	String s4 = s3;
 	s1 = std::move(s4);
 	s2 = String(s1);
-	*/
-	String str, str2;
-
-	std::cout << "fun1" << std::endl;
-	str2 = fun1(str);
-
-	std::cout << "fun2" << std::endl;
-	str2 = fun2(str);
-
-	std::cout << "fun3" << std::endl;
-	str2 = fun3(str);
-
-	std::cout << "fun4" << std::endl;
-	str2 = fun4(str);
-
-	std::cout << "fun5" << std::endl;
-	str2 = fun5(str);
-
-	std::cout << "fun6" << std::endl;
-	str2 = fun6(String());
-
-	std::cout << "fun7" << std::endl;
-	str2 = fun7(String());
-	
+		
 	
 	return 0;
 }
